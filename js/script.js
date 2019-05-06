e381a5bb13f4822285eeb855143a9a0312f3345d
Dennis_Njogu <dcolonel6@gmail.com>
2019-05-06 03:10:00 +0300

    added the toggle functionality in js for 'what we do section'

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $(".hover-effect").hover(handleHoverIn,handleHoverOut);
-    
+    $(".what-we-do").click(handleClick);
 });
 
 function handleHoverIn(evnt){
@@ -15,4 +15,10 @@ function handleHoverOut(evnt){
         "opacity": 0,
         "width": 0
     });
+}
+function handleClick(evnt){
+    $(this).children("img").toggle();
+    $(this).children("div.more-details").toggle()
+    
+
 }
